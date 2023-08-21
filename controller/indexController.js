274d7221bb3c53a057d3f484114143541890c445
MarcoSavarin0 <ateneo2003@gmail.com>
2023-08-21 16:19:54 -0300

    ahora se puede descargar videos hasta 720p

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fetch = require('node-fetch')
 
-async function fetchVideoYt(id) {
+async function fetchMp3Yt(id) {
     try {
         const response = await fetch('https://youtube-mp36.p.rapidapi.com/dl?id=' + id, {
             method: "GET",
@@ -35,6 +35,42 @@ async function fetchSpotify(id){
     }
 }
 
+async function fetchVideoYt(id){
+    try{
+        const response = await fetch('https://ytstream-download-youtube-videos.p.rapidapi.com/dl?id=' + id,
+        {
+            method: "GET",
+            headers: {
+                "x-rapidapi-key": process.env.YT_VIDEO_API_KEY,
+                "x-rapidapi-host": process.env.YT_VIDEO_API_HOST
+            }
+        })
+        const data = await response.json();
+        return data
+    }catch(err){
+        console.log("FetchvideoYt", err);
+        return null
+    }
+}
+
+async function fetchCombinedData(id) {
+    try {
+        const videoData = await fetchVideoYt(id);
+        const mp3Data = await fetchMp3Yt(id);
+
+        const combinedData = {
+            videoData: videoData,
+            mp3Data: mp3Data
+        };
+
+        return combinedData;
+    } catch (error) {
+        console.error("Error fetching combined data:", error);
+        return null;
+    }
+}
+
+
 module.exports = {
     index: function(req,res){
         res.render('index', {spotifySucces: false})
@@ -50,28 +86,35 @@ module.exports = {
         ){
             return res.render('index',{success: false, message : "Please Enter video URL"})
         }else if(urlIDNavegador.length >= 2){
-            fetchVideoYt(urlIDNavegador[1])
-                .then(result  => {
-                    if(result.status === 'ok'){
-                        return res.render('index', {success : true, song_title: result.title, song_link :result.link, idVideo: urlIDNavegador[1]})
+            fetchCombinedData(urlIDNavegador[1])
+            .then(combinedData => {
+                if (combinedData) {
+                    const videoData = combinedData.videoData;
+                    const mp3Data = combinedData.mp3Data;
+                    console.log(videoData.formats);
+                    if (mp3Data.status === 'ok' && videoData.status === 'OK') {
+                        return res.render('index', { success: true, song_title: mp3Data.title, song_link: mp3Data.link, idVideo: urlIDNavegador[1], videoData: videoData.formats});
+                    } else {
+                        return res.render("index", { success: false, message: mp3Data.msg });
                     }
-                    else{
-                        return res.render("index", {success : false, message: result.msg})
-                    }
-                })
-                .catch(err =>{
-                    console.error("Error", err);
-                })
+                } else {
+                    return res.render("index", { success: false, message: "Error fetching combined data" });
+                }
+            })
+            .catch(err => {
+                console.error("Error", err);
+            });
             
         }
         else if(regex.test(videoURL)){
             const lastSlashIndex = videoURL.lastIndexOf("/");
             const afterLastSlash = videoURL.substring(lastSlashIndex + 1);
-            console.log('llegue');
-            fetchVideoYt(afterLastSlash)
-                .then(result => {
-                    if (result.status === 'ok') {
-                        return res.render('index', { success: true, song_title: result.title, song_link: result.link,  idVideo: afterLastSlash });
+            fetchCombinedData(afterLastSlash)
+                .then(combinedData => {
+                    const videoData = combinedData.videoData;
+                    const mp3Data = combinedData.mp3Data;
+                    if (mp3Data.status === 'ok' && videoData.status === 'OK') {
+                        return res.render('index', { success: true, song_title: mp3Data.title, song_link: mp3Data.link,  idVideo: afterLastSlash, videoData: videoData.formats });
                     } else {
                         return res.render("index", { success: false, message: result.msg});
                     }
